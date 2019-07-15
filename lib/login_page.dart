edc3adb312f20890b5d2d5e075a92d7bff638405
SnoopBogg <bsorbun@gmail.com>
2019-07-15 14:28:33 -0500

    adjusting login with a combo of different firebase methods.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,21 +9,35 @@ import 'package:sign_in/validation.dart';
 import 'package:sign_in/authentication.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key key}) : super(key: key);
+//  const LoginScreen({Key key}) : super(key: key);
+
+  LoginScreen({this.auth, this.onSignedIn});
+
+  final BaseAuth auth;
+  final VoidCallback onSignedIn;
 
   @override
-  LoginScreenState createState() => new LoginScreenState();
+  State<StatefulWidget> createState() => new LoginScreenState();
 }
 
 class LoginScreenState extends State<LoginScreen> {
-  BuildContext context;
   final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   ScrollController scrollController = new ScrollController();
-  UserData user = new UserData();
-  UserAuth userAuth = new UserAuth();
   bool autovalidate = false;
   Validations validations = new Validations();
+
+  String _email;
+  String _password;
+  String _errorMessage;
+
+  bool _isLoading;
+
+//  not sure what this is
+  BuildContext context;
+
+  UserData user = new UserData();
+  UserAuth userAuth = new UserAuth();
 
   _onPressed() {
     print("button clicked");
@@ -38,21 +52,41 @@ class LoginScreenState extends State<LoginScreen> {
         .showSnackBar(new SnackBar(content: new Text(value)));
   }
 
-  void _handleSubmitted() {
+  void _handleSubmitted() async {
     final FormState form = formKey.currentState;
+    setState(() {
+      _errorMessage = "";
+      _isLoading = true;
+    });
     if (!form.validate()) {
       autovalidate = true; // Start validating on every change.
       showInSnackBar('Please fix the errors in red before submitting.');
+      setState(() {
+        _errorMessage = "Please fix the errors in red before submitting.";
+        _isLoading = false;
+      });
     } else {
       form.save();
-      userAuth.verifyUser(user).then((onValue) {
-        if (onValue == "Login Successfull")
-          Navigator.pushNamed(context, "/HomePage");
-        else
-          showInSnackBar(onValue);
-      }).catchError((PlatformException onError) {
-        showInSnackBar(onError.message);
-      });
+//      userAuth.verifyUser(user).then((onValue) {
+////        figure out why this is not going through...
+//        if (onValue == "Login Successful") {
+//          Navigator.pushNamed(context, "/HomePage");
+//        } else {
+//          showInSnackBar(onValue);
+//        }}).catchError((PlatformException onError) {
+//        showInSnackBar(onError.message);
+//      });
+      String userId = "";
+      try {
+        userId = await widget.auth.signIn(_email, _password);
+        print('Signed in: $userId');
+        Navigator.pushNamed(context, "/HomePage");
+      } catch (e) {
+        print('Error: $e');
+        setState(() {
+          _isLoading = false;
+        });
+      }
     }
   }
 
@@ -111,6 +145,7 @@ class LoginScreenState extends State<LoginScreen> {
                                   validateFunction: validations.validateEmail,
                                   onSaved: (String email) {
                                     user.email = email;
+                                    _email = email;
                                   }),
                               new InputField(
                                   hintText: "Password",
@@ -125,6 +160,7 @@ class LoginScreenState extends State<LoginScreen> {
                                   validations.validatePassword,
                                   onSaved: (String password) {
                                     user.password = password;
+                                    _password = password;
                                   }),
                               new RoundedButton(
                                 buttonName: "Get Started",
--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,6 @@ import 'package:sign_in/Routes.dart';
 void main() {
   new Routes();
 }
+
+//website that is being referenced
+//https://github.com/tattwei46/flutter_login_demo/blob/master/lib/pages/login_signup_page.dart
