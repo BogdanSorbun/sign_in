656d37fbc04d471b36788df5144d9a365a8034c0
SnoopBogg <bsorbun@gmail.com>
2019-07-15 13:03:21 -0500

    added the rest of the authentication code

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -28,7 +28,6 @@ class UserAuth {
 //      here we see the information from the class that we used earlier...
         email: userData.email, password: userData.password);
     return statusMessage;
-    return user.uid;
   }
 
 //  to verify the new user
@@ -52,5 +51,35 @@ abstract class BaseAuth {
 class Auth implements BaseAuth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  
+  Future<String> signIn(String email, String password) async {
+    FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return user.uid;
+  }
+
+  Future<String> signUp(String email, String password) async {
+    FirebaseUser user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return user.uid;
+  }
+
+  Future<FirebaseUser> getCurrentUser() async {
+    FirebaseUser user = await _firebaseAuth.currentUser();
+    return user;
+  }
+
+  Future<void> signOut() async {
+    return _firebaseAuth.signOut();
+  }
+
+  Future<void> sendEmailVerification() async {
+    FirebaseUser user = await _firebaseAuth.currentUser();
+    user.sendEmailVerification();
+  }
+
+  Future<bool> isEmailVerified() async {
+    FirebaseUser user = await _firebaseAuth.currentUser();
+    return user.isEmailVerified;
+  }
+
 }
