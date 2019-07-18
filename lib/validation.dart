b55f81531f736130da978ee7544675f9b3b20054
Bogdan Sorbun <31483568+BogdanSorbun@users.noreply.github.com>
2019-07-17 20:07:02 -0500

    Update validation.dart
    password confirmation added

--- lib/validation.dart
+++ lib/validation.dart
@@ -2,6 +2,8 @@
 
 class Validations {
 
+  int infoLength = 10;
+
   String validateName(String value) {
     if (value.isEmpty) return 'Name is required.';
     final RegExp nameExp = new RegExp(r'^[A-za-z ]+$');
@@ -19,8 +21,26 @@ class Validations {
   }
 
   String validatePassword(String value) {
+//    make sure not null
     if (value.isEmpty) return 'Password required';
+//    make sure 10 or more characters
+    if (value.length < infoLength) return 'Password needs to be 10 characters';
     return null;
+  }
+
+  String validatePasswordConfirmation(String value) {
+    //    make sure not null
+    if (value.isEmpty) return 'Password required';
+//    make sure 10 or more characters
+    if (value.length < infoLength) return 'Password needs to be 10 characters';
+    return null;
+  }
+
+  bool passwordNotMatch(String firstPasswordValue, String secondPasswordValue) {
+    if (firstPasswordValue != secondPasswordValue) {
+      return false;
+    }
+    return true;
   }
 
   String validateUsername(String value) {
